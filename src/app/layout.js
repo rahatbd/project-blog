065e75bccda07e3ca116a81170ba8643baf20249
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2023-11-21 18:42:09 -0500

    add Exercise 3 solution

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  Work_Sans,
-  Spline_Sans_Mono,
-} from 'next/font/google';
+import { Work_Sans, Spline_Sans_Mono } from 'next/font/google';
 import clsx from 'clsx';
-
-import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
-
+import { BLOG_TITLE, LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './styles.css';
@@ -23,6 +18,11 @@ const monoFont = Spline_Sans_Mono({
   weight: 'variable',
   variable: '--font-family-mono',
 });
+
+export const metadata = {
+  title: BLOG_TITLE,
+  description: 'A wonderful blog about JavaScript',
+};
 
 function RootLayout({ children }) {
   // TODO: Dynamic theme depending on user preference
